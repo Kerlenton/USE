9a89cae6068318a167dac80843970d40cb0f06d4
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2021-06-17 16:57:01 +0400

    Create use_19_21.py

---
+++ use_19_21.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+
+def moves(h):
+    return h*2, h+2
+
+@lru_cache(None)
+
+def f(h):
+    if (h >= 25): 
+        return 'СР'
+        
+    elif any(f(m) == 'СР' for m in moves(h)):
+        return 'П1'
+    
+    elif all(f(m) == 'П1' for m in moves(h)):
+        return 'В1'
+    
+    elif any(f(m) == 'В1' for m in moves(h)):
+        return 'П2'
+    
+    elif all(f(m) == 'П1' or f(m) == 'П2' for m in moves(h)):
+        return 'В2'
+
+for i in range(1, 30):
+    print(i, '->', f(i))
